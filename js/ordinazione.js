5d134472cdb44931efffcf909978287d63ab223c
AresRebatto <aresrebatt@gmail.com>
2023-08-28 18:16:13 +0200

    Implementazione frecce per swipe e altri cibi per ordinazione

--- js/ordinazione.js
+++ js/ordinazione.js
@@ -1,3 +1,7 @@
+//Frecce
+let dx = document.getElementById("rightArrow");
+let sx = document.getElementById("leftArrow");
+
 //Focaccia
 let focacciaNum = document.getElementById("qntFocaccia");
 let boxFocaccia = document.getElementById("focaccia");
@@ -8,24 +12,47 @@ let hotdogNum = document.getElementById("qntHotDog");
 let boxHotdog = document.getElementById("HotDog");
 let costoHotDog = document.getElementById("HotDogPrize");
 
+//Focaccia con salame
+let focacciaSalameNum = document.getElementById("qntFocacciaS");
+let boxFocacciaSalame = document.getElementById("focacciaS");
+let costoFocacciaSalame = document.getElementById("FocacciaSPrize");
+
+//Funzioni frecce
+dx.onclick=function(){
+  //Ancora da scrivere le tabelle che poi andranno al posto
+  //della main-table
+}
+
+sx.onclick=function(){
+  //Ancora da scrivere le tabelle che poi andranno al posto
+  //della main-table
+}
+
 //Funzioni focaccia
-boxFocaccia.addEventListener('click', function() {
-    // specify the action to take when the div is clicked
+boxFocaccia.onclick=function() {
     focacciaNum.innerHTML++;
-  });
+};
 
 function lessFocaccia()
 {
-   focacciaNum.innerHTML-= 2;
+  focacciaNum.innerHTML-= 2;
 }
 
 //funzioni Hot-Dog
-boxHotdog.addEventListener('click', function() {
-    // specify the action to take when the div is clicked
+boxHotdog.onclick=function() {
     hotdogNum.innerHTML++;
-  });
+};
 
 function lessHotDog()
 {
-    hotdogNum.innerHTML-= 2;
+  hotdogNum.innerHTML-= 2;
+}
+
+//Funzioni focaccia salame
+boxFocacciaSalame.onclick=function(){
+  focacciaSalameNum.innerHTML++ ;
+}
+function lessFocacciaS()
+{
+  focacciaSalameNum.innerHTML-=2;
 }
