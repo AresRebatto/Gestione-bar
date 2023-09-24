1c9ba102c41fe933b8272054d599ac52e9d4f6dc
AresRebatto <aresrebatt@gmail.com>
2023-09-24 12:49:30 +0200

    Add dictionary

--- js/ordinazione.js
+++ js/ordinazione.js
@@ -1,118 +1,98 @@
-const costoFocacce = 1.70; //Anche HotDog
-const costoFocaccia = 1.0;
-const cotoletta = 2.00;
-
-function ReturnPrize(objId)
-{
-  let strn = document.getElementById(objId).value;
-  return strn.substring(0,2);
-  // let chr = strn.split('');
-  // return chr[0]+chr[1]+chr[2];
-}
-
+//Tabelle
 let tableOne = document.getElementById("FrstTable");
 let tableTwo = document.getElementById("scndTable");
+
 //Frecce
 let dx = document.getElementById("rightArrow");
 let sx = document.getElementById("leftArrow");
 
-//Focaccia
-let focacciaNum = document.getElementById("qntFocaccia").value;
+//Box alimenti
 let boxFocaccia = document.getElementById("focaccia");
-
-let alimenti = {
-  focaccia : [document.getElementById("qntFocaccia"), 1.00, ReturnPrize("qntFocaccia")]
-};
-
-
-//Hot-dog
-let hotdogNum = document.getElementById("qntHotDog");
 let boxHotdog = document.getElementById("HotDog");
-
-//Focaccia con salame
-let focacciaSalameNum = document.getElementById("qntFocacciaS");
 let boxFocacciaSalame = document.getElementById("focacciaS");
-
-//Focaccia cotto
-let focacciaCottoNum = document.getElementById("qntFocacciaC");
 let boxFocacciaC = document.getElementById("BoxFocacciaC");
-
-//Focaccia mortadella
-//Focaccia cotto
-let focacciaMortNum = document.getElementById("qntFocacciaM");
 let boxFocacciaM = document.getElementById("BoxFocacciaM");
-
-//Panino cotoletta
-let cotolettaNum = document.getElementById("qntCotoletta");
 let boxCotoletta = document.getElementById("BoxCotoletta");
 
+
+let alimenti = {
+  focaccia : [document.getElementById("qntFocaccia"), 1.00],
+  hotdog: [ document.getElementById("qntHotDog"), 1.70],
+  focacciaSalame: [document.getElementById("qntFocacciaS"), 1.70],
+  focacciaCotto: [document.getElementById("qntFocacciaC"), 1.70],
+  focacciaMort: [document.getElementById("qntFocacciaM"), 1.70],
+  cotoletta: [document.getElementById("qntCotoletta"), 2.00],
+};
 
 //Funzioni focaccia
 boxFocaccia.onclick=function() {
   alimenti.focaccia[0].innerHTML++;
-  console.log(alimenti.focaccia[2]);
 };
 
 function lessFocaccia()
 {
-  focacciaNum.innerHTML-= 2;
+  alimenti.focaccia[0].innerHTML-=2;
 }
 
 //funzioni Hot-Dog
 boxHotdog.onclick=function() {
-    hotdogNum.innerHTML++;
+  alimenti.hotdog[0].innerHTML++;
 };
 
 function lessHotDog()
 {
-  hotdogNum.innerHTML-= 2;
+  alimenti.hotdog[0].innerHTML-=2;
 }
 
 //Funzioni focaccia salame
 boxFocacciaSalame.onclick=function(){
-  focacciaSalameNum.innerHTML++ ;
+  alimenti.focacciaSalame[0].innerHTML++;
 }
 function lessFocacciaS()
 {
-  focacciaSalameNum.innerHTML-=2;
+  alimenti.focacciaSalame[0].innerHTML-=2;
 }
 
 //Funzioni focaccia cotto
 boxFocacciaC.onclick=function() {
-    focacciaCottoNum.innerHTML++;
+  alimenti.focacciaCotto[0].innerHTML++;
 };
 
 function lessFocacciaC()
 {
-  focacciaCottoNum.innerHTML-= 2;
+  alimenti.focacciaCotto[0].innerHTML-=2;
 }
 
 //Funzioni focaccia mortadella
 boxFocacciaM.onclick=function() {
-  focacciaMortNum.innerHTML++;
+  alimenti.focacciaMort[0].innerHTML++;
 };
 
 function lessFocacciaM()
 {
-  focacciaMortNum.innerHTML-= 2;
+  alimenti.focacciaMort[0].innerHTML-=2;
 }
 
 //Funzioni panino cotoletta
 boxCotoletta.onclick=function() {
-  cotolettaNum.innerHTML++;
+  alimenti.cotoletta[0].innerHTML++;
 };
 
 function lessCotoletta()
 {
-  cotolettaNum.innerHTML-= 2;
+  alimenti.cotoletta[0].innerHTML-=2;
 }
 
 function ConfermaOrdine()
 {
   document.getElementById("cover").style.visibility = "visible";
   document.getElementById("popup").style.visibility = "visible";
-  let totale = focacciaNum*costoFocaccia;
-  document.getElementById("costoTotale").innerHTML= parseFloat(totale);
+  let totale = 0;
+  for(let [key, value] of Object.entries(alimenti))
+  {
+    totale += value[0]*value[1];
+  }
+  document.getElementById("costoTotale").innerHTML= totale;
 }
 
 function home()
